7e8849be9eeb5c58cad7bc6bf0b5453ecb4fe4a9
Erin Eland <erin@erineland.com>
2017-12-09 23:05:28 +0100

    Pulling car data down from API and randomly showing a new car on each button press.

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CarPanel from './CarPanel/CarPanel.jsx';
 import SwitchVehicleButton from './SwitchVehicleButton/SwitchVehicleButton.jsx';
+import axios from 'axios';
 
 class App extends Component {
 
@@ -9,33 +10,70 @@ class App extends Component {
 
     //explicitly bind hoisted functions to this on lexical scope.
     this.switchCar = this.switchCar.bind(this);
-    
+
     const defaultCar = {
       discount: 10,
       vehicleType: 'SUV',
       price: '40',
-      make: 'Honda',
-      model: 'CRV',
-      logo: '/dist/images/hertz-logo.jpg'
+      make_model: 'Honda CRV',
+      provider: 'hertz'
     };
-    
-    this.state = { carToShow: defaultCar };
+
+    this.state = {
+      carToShow: defaultCar
+    };
   }
 
   switchCar() {
 
-    const newCarToShow = {
-      discount: null,
-      vehicleType: 'Compact',
-      price: '28.50',
-      make: 'Ford',
-      model: 'Ka',
-      logo: '/dist/images/avis-logo.jpg'
-    }
+    //Get car info
+    axios.get('/api/cars')
+      .then((response) => {
+        let carsFromApi = [];
 
-    this.setState({
-      carToShow: newCarToShow
-    })
+        //Apply discounts to car prices, if a discount is provided.
+        if (response.data.discount_percentage) {
+          const discountPercentage = response.data.discount_percentage;
+
+          carsFromApi = response.data.results.map((car) => {
+            const discountToSubtract = car.price * (discountPercentage * 0.01);
+            const newCarPrice = car.price - discountToSubtract;
+            const roundedNewCarPrice = Math.round(newCarPrice * 100) / 100; //Round the new car price to 2 d.p
+            car.discount = discountPercentage;
+            car.price = roundedNewCarPrice;
+            return car;
+          })
+        } else {
+          carsFromApi = response.data.results;
+        }
+
+        //Choose a car at random to display between 0 and 4
+        let randomCarIndexToShow = Math.floor(Math.random() * 5);
+
+        //Ensure we are not showing the same car as previous click
+        let nextCarToShow;
+        let sameCarAsLastTime = true;
+
+        //Choose a random car, check that we won't be showing the same car twice.
+        do {
+          nextCarToShow = carsFromApi[randomCarIndexToShow];
+
+          //If car make and model are same, choose another at random and re-check
+          if (this.state.carToShow.make_model == nextCarToShow.make_model) {
+              randomCarIndexToShow = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+          } else {
+            sameCarAsLastTime = false;
+          }
+        } while (sameCarAsLastTime);
+
+        //Update the UI with the new, randomly chosen car
+        this.setState({
+          carToShow: nextCarToShow
+        })
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   render() {
--- src/client/components/CarPanel/CarPanel.jsx
+++ src/client/components/CarPanel/CarPanel.jsx
@@ -3,18 +3,21 @@ import styles from './carPanel.scss';
 class CarPanel extends Component {
     render() {
 
-        //conditionally render a discount if present
+        //Conditionally render a discount if present
         let discount = null;
         if (this.props.car.discount) {
             discount = <div className="discount-panel">Save {this.props.car.discount}%</div>
         }
 
+        //Generate the logo img src url using car provider
+        const carProviderLogo = `/dist/images/${this.props.car.provider}-logo.jpg`;
+        
         return <div className="car-panel">
             {discount}
-            <img className="make-logo" src={this.props.car.logo}></img>
+            <img className="make-logo" src={carProviderLogo}></img>
             <hr className="panel-divider"></hr>
             <div className="car-details">
-                <div className="make-model">{this.props.car.make} {this.props.car.model}</div>
+                <div className="make-model">{this.props.car.make_model}</div>
                 <div className="vehicle-type">{this.props.car.vehicleType}</div>
             </div>
             <div className="price">£{this.props.car.price}</div>
