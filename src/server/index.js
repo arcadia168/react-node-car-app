718500961141c21e256390017ccb9217ebf38e89
Erin Eland <erin@erineland.com>
2017-12-11 13:25:15 +0100

    Altering routes so that they work on Azure

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express();
 
 var Providers = ["avis", "budget", "enterprise", "hertz"];
-var VehicleTypes = { "C": "Compact", "S": "SUV", "E": "Estate" };
+var VehicleTypes = {
+  "C": "Compact",
+  "S": "SUV",
+  "E": "Estate"
+};
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -11,7 +15,7 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-var Vehicle = function(make, vehicle, provider, price) {
+var Vehicle = function (make, vehicle, provider, price) {
   return {
     "make_model": make + " " + vehicle["model"],
     "type": vehicle["type"],
@@ -20,27 +24,27 @@ var Vehicle = function(make, vehicle, provider, price) {
   };
 }
 
-Vehicle.random = function() {
+Vehicle.random = function () {
   function getRandomLong(min, max) {
-      var longVal = (Math.random() * (max - min)) + min;
-      return Math.floor(longVal * 100)/100;
+    var longVal = (Math.random() * (max - min)) + min;
+    return Math.floor(longVal * 100) / 100;
   }
 
   function chooseRandomPropertyKey(obj) {
-     var max = Object.keys(obj).length;
-     return Object.keys(obj)[getRandomInt(0, max)];
+    var max = Object.keys(obj).length;
+    return Object.keys(obj)[getRandomInt(0, max)];
   }
 
   var make = chooseRandomPropertyKey(vehicles)
   var makeCarList = vehicles[make];
   var price = getRandomLong(30, 100);
-  var provider = Providers[getRandomInt(0, Providers.length -1)]
+  var provider = Providers[getRandomInt(0, Providers.length - 1)]
   var vehicle = makeCarList[chooseRandomPropertyKey(makeCarList)];
 
   return new Vehicle(make, vehicle, provider, price);
 }
 
-Vehicle.randomList = function(numberOfVehicles) {
+Vehicle.randomList = function (numberOfVehicles) {
   var vehicleList = [];
   for (var i = 0; i < numberOfVehicles; i++) {
     vehicleList.push(this.random());
@@ -49,35 +53,57 @@ Vehicle.randomList = function(numberOfVehicles) {
 }
 
 var vehicles = {
-  "Ford" : {
-      "K" : { model: "Ka", type: VehicleTypes["C"] },
-      "F": { model: "Fiesta", type: VehicleTypes["C"] },
-      "U": { model: "Kuga", type: VehicleTypes["S"] },
+  "Ford": {
+    "K": {
+      model: "Ka",
+      type: VehicleTypes["C"]
+    },
+    "F": {
+      model: "Fiesta",
+      type: VehicleTypes["C"]
+    },
+    "U": {
+      model: "Kuga",
+      type: VehicleTypes["S"]
+    },
   },
-  "Audi" : {
-      "3" : { model: "A3", type: VehicleTypes["C"] },
-      "Q": { model: "Q5", type: VehicleTypes["S"] }
+  "Audi": {
+    "3": {
+      model: "A3",
+      type: VehicleTypes["C"]
+    },
+    "Q": {
+      model: "Q5",
+      type: VehicleTypes["S"]
+    }
   },
   "Vauxhall": {
-    "C": { model: "Corsa", type: VehicleTypes["C"] },
-    "M": { model: "Mokka", type: VehicleTypes["S"] },
-    "I": { model: "Insignia", type: VehicleTypes["E"] }
+    "C": {
+      model: "Corsa",
+      type: VehicleTypes["C"]
+    },
+    "M": {
+      model: "Mokka",
+      type: VehicleTypes["S"]
+    },
+    "I": {
+      model: "Insignia",
+      type: VehicleTypes["E"]
+    }
   }
 }
 
-app.use(express.static(__dirname +'./../../'));
-app.use('/resources/jquery', express.static(__dirname + '../../node_modules/jquery/dist'));
-app.use('/resources/css', express.static(__dirname + '../../dist/css'));
-app.use('/resources/js', express.static(__dirname + '../../dist/js'));
-app.use('/resources/images', express.static(__dirname + '../../dist/images'));
+app.use(express.static(__dirname + './../../'));
+app.use('/dist/images', express.static(__dirname + '../../dist/images'));
 app.use('/dist/bundle.js', express.static(__dirname + '../../dist/bundle.js'));
 
-app.get('/api/cars', function (req, res) {
-  var responseJson = {};
-  responseJson.discount_percentage = getRandomInt(0, 20);
-  responseJson.results = Vehicle.randomList(5);
-  res.send(responseJson);
-});
+app.route('/api/cars')
+  .get(function (req, res) {
+    var responseJson = {};
+    responseJson.discount_percentage = getRandomInt(0, 20);
+    responseJson.results = Vehicle.randomList(5);
+    res.send(responseJson);
+  });
 
 app.listen(3000, function () {
   console.log('Rentalcars.com FED pair programming exercise app listening on port 3000!')
